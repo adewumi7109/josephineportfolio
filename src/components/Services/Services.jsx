06e6f14c251d1690c9ad4339a2bc473b90b78661
Adewumi <ogunsanyaadewumi01@gmail.com>
2025-03-10 18:56:31 +0100

    fixed rwd

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,61 +1,61 @@
-import React from 'react'
-import './services.css'
+import React from "react";
+import "./services.css";
 
-
-const serviceData =[
+const serviceData = [
   {
-    icon: '/icons/language.png',
-    title: 'UI/UX & Web Design',
-    content: 'Designing user-friendly websites and apps that look great and work seamlessly'
-
+    icon: "/icons/language.png",
+    title: "UI/UX & Web Design",
+    content:
+      "Designing user-friendly websites and apps that look great and work seamlessly",
   },
   {
-    icon: '/icons/branding_watermark.png',
-    title: 'Branding & Identity',
-    content: 'Crafting unique logos, color palettes, and brand visuals that stand out'
-
+    icon: "/icons/branding_watermark.png",
+    title: "Branding & Identity",
+    content:
+      "Crafting unique logos, color palettes, and brand visuals that stand out",
   },
   {
-    icon: '/icons/phone_android.png',
-    title: 'Mobile App UI/UX',
-    content: 'Creating intuitive mobile experiences that keep users engaged'
-
+    icon: "/icons/phone_android.png",
+    title: "Mobile App UI/UX",
+    content: "Creating intuitive mobile experiences that keep users engaged",
   },
   {
-    icon: '/icons/design_services.png',
-    title: 'Print & Marketing Design',
-    content: 'Designing flyers, social media graphics, and packaging that make an impact'
-
+    icon: "/icons/design_services.png",
+    title: "Print & Marketing Design",
+    content:
+      "Designing flyers, social media graphics, and packaging that make an impact",
   },
   {
-    icon: '/icons/edit_note.png',
-    title: 'Wireframing & Prototyping',
-    content: 'Turning ideas into interactive wireframes and user-friendly flows'
-
+    icon: "/icons/edit_note.png",
+    title: "Wireframing & Prototyping",
+    content:
+      "Turning ideas into interactive wireframes and user-friendly flows",
   },
   {
-    icon: '/icons/computer.png',
-    title: 'Design Systems & UI Kits',
-    content: 'Creating reusable components for consistent designs'
-
+    icon: "/icons/computer.png",
+    title: "Design Systems & UI Kits",
+    content: "Creating reusable components for consistent designs",
   },
-]
+];
 function Services() {
   return (
-    <section id='services'>
-
-        <h1>Services</h1>
-        <div className='wrapper service-cont'>
-          {serviceData.map((item, index)=>(
-            <div className='box' key={index}>
-              <img src={item?.icon} alt={item?.icon} />
+    <section id="services">
+      <h1>Services</h1>
+      <div className="wrapper service-cont">
+        {serviceData.map((item, index) => (
+          <div className="box" key={index}>
+            <img src={item?.icon} alt={item?.icon} />
+            <div>
               <h2>{item?.title}</h2>
+            </div>
+            <div>
               <p>{item?.content}</p>
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
+      </div>
     </section>
-  )
+  );
 }
 
-export default Services
+export default Services;
